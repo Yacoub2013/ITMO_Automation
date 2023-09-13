343f8fc4ff37f016716a6cd4a3f6f8ec6687fefd
Mustafa <143730862+Yacoub2013@users.noreply.github.com>
2023-09-13 15:14:13 +0300

    5-corrected- commit

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -1,28 +1,20 @@
 from selenium import webdriver
-import time
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 
-driver = webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
-time.sleep(3)
-suarech_username = driver.find_element(By.CSS_SELECTOR, '#user-name').accessible_name
-suarech_username_1="Username"
+def test_site_visit():
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+
+    try:
+        driver.find_element(By.CSS_SELECTOR, '#user-name')
+        driver.find_element(By.CSS_SELECTOR, '#password')
+        driver.find_element(By.CSS_SELECTOR, '#login-button')
+    except NoSuchElementException:
+        print('Элементы не найдены')
+        assert False
+    assert True
+    print('Элементы найдены')
 
 
-driver = webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
-time.sleep(3)
-suarech_password = driver.find_element(By.CSS_SELECTOR, '#password').accessible_name
-suarech_password_1="Password"
-
-
-driver = webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
-time.sleep(3)
-suarech_submit = driver.find_element(By.CSS_SELECTOR, '#login-button').accessible_name
-suarech_supmit_1="Login"
-
-
-if suarech_username ==  suarech_username_1 and  suarech_password ==  suarech_password_1 and  suarech_submit ==  suarech_supmit_1:
-    print("Элементы найдены")
